f8e39cde1f88d6a82b1fa7ae2a32f01aa6abd9ab
another-clear-blue-sky <another.clear.blue.sky@gmail.com>
2025-07-14 13:49:29 -0500

    updates to xss-examples

--- vulnerabilities/xss_r/source/medium.php
+++ vulnerabilities/xss_r/source/medium.php
@@ -2,13 +2,13 @@
 
 header ("X-XSS-Protection: 0");
 
-//require_once("../utils.php");
+require_once("../utils.php");
 
 // Is there any input?
 if( array_key_exists( "name", $_GET ) && $_GET[ 'name' ] != NULL ) {
 	// Get input
 	$name = str_replace( '<script>', '', $_GET[ 'name' ] );
-	// $name = clean_html($_GET[ 'name' ] );
+	$name = clean_html($_GET[ 'name' ] );
 	// Feedback for end user
 	$html .= "<pre>Hello {$name}</pre>";
 }
