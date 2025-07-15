69a369f99c77278a449c688ff00ce4e3708834a2
another-clear-blue-sky <another.clear.blue.sky@gmail.com>
2025-07-15 14:50:50 -0500

    updated to fix xss-r

--- vulnerabilities/xss_r/source/low.php
+++ vulnerabilities/xss_r/source/low.php
@@ -2,14 +2,14 @@
 
 header ("X-XSS-Protection: 0");
 
-//include_once("../utils.php");
+include_once("../utils.php");
 
 // Is there any input?
 if( array_key_exists( "name", $_GET ) && $_GET[ 'name' ] != NULL ) {
 	// Feedback for end user
-	// $target = clean_web($_GET[ 'name' ]);
-	// $html .= '<pre>Hello ' . $target . '</pre>';
-	$html .= '<pre>Hello ' . $_GET[ 'name' ] . '</pre>'; // comment for fix
+	$target = clean_web($_GET[ 'name' ]);
+	$html .= '<pre>Hello ' . $target . '</pre>';
+	//$html .= '<pre>Hello ' . $_GET[ 'name' ] . '</pre>'; // comment for fix
 }
 
 ?>
